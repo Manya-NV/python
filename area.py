34584070648ccfdf38ceda083d4259876602a482
Manya N V <manyanv44@gmail.com>
2026-07-25 20:13:46 +0530

    Delete area.py

--- area.py
+++
@@ -1,5 +0,0 @@
-# Calculate the area of a rectangle.
-length=float(input("enter the length of the rectangle: "))
-breadth=float(input("enter the breadth of the rectangle: "))
-area=length*breadth
-print("area of rectangle is: ",area)
