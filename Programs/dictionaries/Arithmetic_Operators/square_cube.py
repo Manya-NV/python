9e52f06f6f592f992fda593c3984ae78fdb60177
Manya-NV <manyanv44@gmail.com>
2026-07-25 15:24:38 +0530

    deleted

--- Programs/dictionaries/Arithmetic_Operators/square_cube.py
+++
@@ -1,5 +0,0 @@
-#Find the square and cube of a number.
-x=float(input("enter the numbers want to be squared and cubed "))
-sq=x**2
-cb=x**3
-print(f"suqare and cube of {x} is {sq} and {cb}")
--- Programs/dictionaries/Arithmetic_Operators/swap.py
+++
@@ -1,5 +0,0 @@
-#•	Swap two numbers without a third variable.
-a=int(input("enter the number a: "))
-b=int(input("enter the number b: "))
-a,b=(b,a)
-print(f"a={a},b={b}")
